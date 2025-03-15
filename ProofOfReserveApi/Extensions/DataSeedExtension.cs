4ec34cc058a31d9f30d4ca43384bd9f483b33283
Azizov Timur <timmik94@gmail.com>
2025-03-15 17:30:57 +0900

    Adde balance generation method to be able to handle bigger users number.

--- ProofOfReserveApi/Extensions/DataSeedExtension.cs
+++ ProofOfReserveApi/Extensions/DataSeedExtension.cs
@@ -13,10 +13,23 @@ public static class DataSeedExtension
     {
         var storage = app.Services.GetRequiredService<IUserBalanceStorage>();
 
-        for(int i = 1; i <= usersCount; i++)
+        for (int i = 1; i <= usersCount; i++)
         {
-            string dataString = $"({i},{i}{i}{i}{i})";
+            string balance = GenerateBalanceValue(i);
+            string dataString = $"({i},{balance})";
             storage.AddOrUpdateUser(dataString);
         }
+    }
+
+    private static string GenerateBalanceValue(int currentUserId)
+    {
+        string balance = string.Empty;
+        do
+        {
+            balance += currentUserId.ToString();
+        } 
+        while (balance.Length < 4); //We want balance walues for around 4 digits long.
+
+        return balance;
     }
 }
